1b73a1d2b052a1ac9e862c9ee57fdba34aec6b53
laurielim <laurie.limsam@gmail.com>
2021-06-30 11:09:42 +0300

    1.4: course information step4

--- part1/courseinfo/src/components/Total.js
+++ part1/courseinfo/src/components/Total.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-const Total = ({exercises1, exercises2, exercises3}) => {
-    return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+const Total = ({parts}) => {
+    let total = 0;
+    parts.forEach((part)=>total+=part.exercises)
+    return <p>Number of exercises {total}</p>
 }
 
 export default Total
