6c48970d271ee6d3761d18df7b68ff41aafff2c6
laurielim <laurie.limsam@gmail.com>
2021-07-21 12:40:51 +0300

    unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,32 @@
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return<></>;
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+    <h1>UniCafe Feedback</h1>
+    <section>
+      <h2>Give feedback</h2>
+      <button onClick={()=>setGood(good+1)}>good</button>
+      <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
+      <button onClick={()=>setBad(bad+1)}>bad</button>
+    </section>
+    <section>
+      <h2>Statistics</h2>
+      <ul>
+        <li>good {good}</li>
+        <li>neutral {neutral}</li>
+        <li>bad {bad}</li>
+      </ul>
+    </section>
+      
+    </>
+  )
 }
 
 export default App;
