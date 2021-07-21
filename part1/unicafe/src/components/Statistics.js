a03493ae8d6f42332237a72515802efaec9c0a68
laurielim <laurie.limsam@gmail.com>
2021-07-21 13:06:54 +0300

    unicafe step4: Conditionally render stats

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,17 +1,22 @@
 import React from "react";
 
 const Statistics = ({good, neutral, bad, all}) => {
-    return <section>
-      <h2>Statistics</h2>
-      <ul>
+
+const stats = () => (
+    <ul>
         <li>good {good}</li>
         <li>neutral {neutral}</li>
         <li>bad {bad}</li>
         <li>all {all}</li>
-        <li>average {((good * 1) + (bad * - 1))/all ? ((good * 1) + (bad * - 1))/all : 0}</li>
-        <li>positve {((good * 1) / all) * 100 ? ((good * 1) / all) * 100 : 0}%</li>
-      </ul>
-    </section>
+        <li>average {((good * 1) + (bad * - 1))/all}</li>
+        <li>positve {((good * 1) / all) * 100}%</li>
+    </ul>)
+
+    return (
+    <section>
+      <h2>Statistics</h2>
+      {all ? stats() : <p>No feedbback give</p>}
+    </section>)
 
 
   }
