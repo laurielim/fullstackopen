d22c6efb92f2394618e8958fcdea21a0de2be34a
laurielim <laurie.limsam@gmail.com>
2021-07-21 12:54:24 +0300

    unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,15 +6,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const update = (state, setFn) => {
+    setFn(state + 1);
+    setAll(all + 1);
+  }
+  
 
   return (
     <>
     <h1>UniCafe Feedback</h1>
     <section>
       <h2>Give feedback</h2>
-      <button onClick={()=>setGood(good+1)}>good</button>
-      <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
-      <button onClick={()=>setBad(bad+1)}>bad</button>
+      <button onClick={()=>update(good, setGood)}>good</button>
+      <button onClick={()=>update(neutral, setNeutral)}>neutral</button>
+      <button onClick={()=>update(bad, setBad)}>bad</button>
     </section>
     <section>
       <h2>Statistics</h2>
@@ -22,6 +28,9 @@ const App = () => {
         <li>good {good}</li>
         <li>neutral {neutral}</li>
         <li>bad {bad}</li>
+        <li>all {all}</li>
+        <li>average {((good * 1) + (bad * - 1))/all ? ((good * 1) + (bad * - 1))/all : 0}</li>
+        <li>positve {((good * 1) / all) * 100 ? ((good * 1) / all) * 100 : 0}%</li>
       </ul>
     </section>
       
