d7a73828949a6282434ab9f73f8bb07285bc8434
laurielim <laurie.limsam@gmail.com>
2021-07-27 15:44:03 +0300

    handle events

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,9 +1,36 @@
-import React from "react"
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleChange = (e) => setNewName(e.target.value)
+
+  const addName = (e) => {
+    e.preventDefault()
+    setPersons([...persons, {name: newName}])
+    setNewName('')
+  }
+
   return (
-    <div className="App"></div>
-  );
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button onClick={addName} type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person=><li key={person.name}>{person.name}</li>)}
+      </ul>
+    </div>
+  )
 }
 
-export default App;
+export default App
