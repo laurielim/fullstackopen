bf6180e9904af40517e6cc25d7e89e655577f864
laurielim <laurie.limsam@gmail.com>
2021-07-27 14:27:43 +0300

    step 9 allow for an arbitrary number of courses

--- part2/courseinfo2/src/components/Total.js
+++ part2/courseinfo2/src/components/Total.js
@@ -5,7 +5,7 @@ const Total = ({ course }) => {
     const totalExercises = course.parts.map((part)=>part.exercises).reduce((sum,rest)=>sum+rest,0);
 
     return(
-      <p>total of exercises {totalExercises}</p>
+      <p><strong>total of {totalExercises} exercises</strong></p>
     ) 
   }
 
