e160f9bd25a7d4bbcc94d36fa75f5d1e990e3336
laurielim <laurie.limsam@gmail.com>
2022-12-10 15:11:25 +0200

    Cleanup and remove person array

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -21,29 +21,6 @@ app.use(
 
 app.use(cors());
 
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-];
-
 app.get("/", (req, res) => {
 	res.send("Hello World!!!");
 });
@@ -77,8 +54,8 @@ app.post("/api/persons", (req, res) => {
 
 	const newContact = { ...req.body };
  	const contact = new Contact({...req.body})
-		contact.save().then(savedContact => {
-			res.json(savedContact);
+	contact.save().then(savedContact => {
+		res.json(savedContact);
 	})
 });
 
